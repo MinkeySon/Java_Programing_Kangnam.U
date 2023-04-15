361c5d05a7c0ba1d97729739ea13cbdcc9564a14
HandMK <126847381+MinkeySon@users.noreply.github.com>
2023-04-15 15:55:47 +0900

    Update Interface실습_14번.java

--- Interface실습_14번.java
+++ Interface실습_14번.java
@@ -1,70 +1,64 @@
 package W5;
 
-interface Shape{
-	void redraw();
-	double getArea();	
+interface Shapess {
+    final double PI = 3.14; // 상수
+    void draw(); // 도형을 그리는 추상 메소드
+    double getArea(); // 도형의 면적을 리턴하는 추상 메소드
+    default public void redraw() { // 디폴트 메소드
+        System.out.print("--- 다시 그립니다.");
+        draw();
+    }
 }
 
-class Circle implements Shape{
-	private int r;
-	
-	Circle(int r) {
-		this.r = r;
-	}
-	
-	public void redraw() {
-		System.out.println("--- 다시 그립니다. 반지름이"+ r + "인 원입니다.");
-	}
-	public double getArea() {
-		return (r*r*3.14);
-	}
+class Circlecc implements Shapess{
+    private int radius;
+    public Circlecc(int radius) {
+        this.radius = radius;
+    }
+    public void draw() {
+        System.out.println("반지름이 "+radius+"인 원입니다.");
+    }
+    public double getArea() {
+        return PI*radius*radius;
+    }
 }
 
-class Oval implements Shape{
-	private int a, b;
-	
-	Oval(int a, int b){
-		this.a = a;
-		this.b = b;
-	}
-	
-	public void redraw() {
-		System.out.println("--- 다시 그립니다."+a+"x"+b+"에 내접하는 타원입니다.");
-	}
-	
-	public double getArea() {
-		double area = a * b * 3.14;
-		return Double.parseDouble(String.format("%.13f", area));
-	}
+class Oval implements Shapess{
+    private int a,b;
+    public Oval(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public void draw() {
+        System.out.println(a + "x" + b + "에 내접하는 타원입니다.");
+    }
+    public double getArea() {
+        return PI*a*b;
+    }
 }
 
-class Rect implements Shape{
-	private int a, b;
-	
-	Rect(int a, int b){
-		this.a = a;
-		this.b = b;
-	}
-	
-	public void redraw() {
-		System.out.println("--- 다시 그립니다."+a+"x"+b+"에 내접하는 사각형입니다.");
-	}
-	
-	public double getArea() {
-		return a*b;
-	}
+class Rectrr implements Shapess{
+    private int a, b;
+    public Rectrr(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public void draw() {
+        System.out.println(a + "x" + b + "크기의 사각형.");
+    }
+    public double getArea() {
+        return a*b;
+    }
 }
-
 public class Interface실습_14번 {
 
 	public static void main(String[] args) {
-		Shape [] list = new Shape[3];
-		list[0] = new Circle(10);
-		list[1] = new Oval(20,30);
-		list[2] = new Rect(10,40);
-		
-		for(int i=0; i<list.length; i++) list[i].redraw(); //메소드 선언만 해서 return 값이 필요없음.
-		for(int i=0; i<list.length; i++) System.out.println("면적은" + list[i].getArea()); //println이라서 return 값만 필요
+		Shapess[] list = new Shapess[3]; // Shape을 상속받은 클래스 객체의 레퍼런스 배열
+        list[0] = new Circlecc(10); // 반지름이 10인 원 객체
+        list[1] = new Oval(20, 30); // 20x30 사각형에 내접하는 타원
+        list[2] = new Rectrr(10, 40); // 10x40 크기의 사각형
+        for(int i=0; i<list.length; i++) list[i].redraw();
+        for(int i=0; i<list.length; i++) System.out.println("면적은 "+ list[i].getArea());
 	}
 
 }
